93a7f1469d8a018b8a5f7e38955204c889810ab5
Denis Cappellin <denis@cappell.in>
2019-04-09 21:43:45 +0200

    Rewrite if mounted condition using the exit code

--- mount_lacie_cloudbox.sh
+++ mount_lacie_cloudbox.sh
@@ -8,11 +8,11 @@ if ( $1 != "" ) set USERNAME = $1
 set NAS          = "lacie-cloudbox.local"
 set SERVICE      = "LaCie-CloudBox"
 set PASSWORD     = `security find-generic-password -wa $USERNAME -s $SERVICE`
-set MOUNT_FOLDER = /private/tmp/${SERVICE}_${USERNAME}
+set MOUNT_FOLDER = /private/tmp/${SERVICE}_$USERNAME
 
 # Remove the mounting directory, if exists
 if ( ! -d $MOUNT_FOLDER ) mkdir $MOUNT_FOLDER
 
-if ( `mount | grep -c "on ${MOUNT_FOLDER}"` == 0 ) then
-	mount_afp -o nobrowse afp://${USERNAME}:${PASSWORD}@${NAS}/${USERNAME} $MOUNT_FOLDER
+if ( ! { ( mount | grep -q "on $MOUNT_FOLDER" ) } ) then
+	mount_afp -o nobrowse afp://${USERNAME}:$PASSWORD@$NAS/$USERNAME $MOUNT_FOLDER
 endif
